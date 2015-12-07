9738059f09ff01be359d8b5efee8d05c4bac77c1
Roberto_Bandeira_de_Mello_Morais_da_Silva <ee106a-az@m92p-8>
2015-12-07 01:50:15 -0800

    python script changes hard coded paths

--- change_hard_paths.py
+++ change_hard_paths.py
@@ -2,19 +2,26 @@ import fileinput
 import os
 import sys
 
-dependent_paths = ['src/ar_track_alvar/launch/webcam_track.launch',
+yml_dependent_paths = ['src/ar_track_alvar/launch/webcam_track.launch',
                     'src/homography/launch/run_cam.launch']
+
 proj_path =  os.path.dirname(os.path.realpath(__file__)) + '/'
 
 print proj_path
 
 
-for path in dependent_paths:
+for path in yml_dependent_paths:
     for line in fileinput.input(path, inplace=1):
         if 'name="camera_info_url"' in line:
             print '<param name="camera_info_url" value="file:///' + proj_path + 'lifecam.yml" />'
         else:
             print line,
+
+for line in fileinput.input('scripts/set_env.sh', inplace=1):
+    if 'setup.bash' in line:
+        print "source " + proj_path + "devel/setup.bash"
+    else:
+        print line,
 
 
 
