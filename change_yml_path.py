e025b4557c749eb55c0b76985722bfcc7edc2290
Roberto_Bandeira_de_Mello_Morais_da_Silva <ee106a-az@m92p-8>
2015-12-07 01:40:27 -0800

    python script to change yml paths, gitignore entry for *~

--- change_yml_path.py
+++ change_yml_path.py
@@ -1,8 +1,20 @@
 import fileinput
 import os
+import sys
 
-proj_path =  os.path.dirname(os.path.realpath(__file__))
+dependent_paths = ['src/ar_track_alvar/launch/webcam_track.launch',
+                    'src/homography/launch/run_cam.launch']
+proj_path =  os.path.dirname(os.path.realpath(__file__)) + '/'
 
-for launch_path in fileinput.input('txts/yml_dependencies_path.txt', inplace=1):
-    file_being_edited = fileinput.input(launch_path, inplace=1)
-    print file_being_edited.filename()
+print proj_path
+
+
+for path in dependent_paths:
+    for line in fileinput.input(path, inplace=1):
+        if 'name="camera_info_url"' in line:
+            print '<param name="camera_info_url" value="file:///' + proj_path + 'lifecam.yml" />'
+        else:
+            print line,
+
+
+
